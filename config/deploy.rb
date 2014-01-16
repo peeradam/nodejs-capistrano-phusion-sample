8913592d4980968dd59d03570206921aac4e237c
Benno van den Berg <bennovandenberg@gmail.com>
2014-01-16 15:28:20 +0100

    Set log level and pretty format

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :ssh_options, {
   user: "www-data"
 }
 
-# set :format, :pretty
-# set :log_level, :debug
+set :format, :pretty
+set :log_level, :debug
 # set :pty, true
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
