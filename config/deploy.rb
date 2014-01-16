452e1b918921a3a973caf805e1f2c8009abfbbd9
Benno van den Berg <bennovandenberg@gmail.com>
2014-01-16 15:41:16 +0100

    Uncomment touch command

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :mkdir, '-p', release_path.join('tmp')
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
