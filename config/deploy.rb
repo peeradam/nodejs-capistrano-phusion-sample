fc8cc22bb5ac6d3f471e16f562a0195b7ea84619
Benno van den Berg <bennovandenberg@gmail.com>
2014-01-16 15:17:57 +0100

    Fall back to touching restart again

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,8 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      #execute :touch, release_path.join('tmp/restart.txt')
-      execute "passenger-config", "restart-app", current_path
+      execute :mkdir, '-p', release_path.join('tmp')
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
