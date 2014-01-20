f58cf7fce1a015b7199ec41d67602c53dc0a436a
Benno van den Berg <bennovandenberg@gmail.com>
2014-01-20 12:34:27 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,4 @@
 set :application, 'my_app_name'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
 set :scm, :copy
 
 set :ssh_options, {
@@ -12,40 +7,25 @@ set :ssh_options, {
 
 set :format, :pretty
 set :log_level, :debug
-# set :pty, true
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), wait: 5 do
       # Your restart mechanism here, for example:
       execute :mkdir, '-p', release_path.join('tmp')
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-  
+  desc 'Rebuild modules using npm rebuild'
   before :updated, :rebuild_npm do
     on roles(:app) do
       execute :npm, "rebuild"
     end
   end
 
+  after :finishing, 'deploy:cleanup'
+  
 end
-
